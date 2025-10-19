623cc10a011c943e7bd37d45683a5027e415adbe
Jonathan Morgan <twig2let@gmail.com>
2025-10-19 18:37:31 +0100

    Adds roku-deploy script

---
+++ scripts/roku-deploy.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const rokuDeploy = require('roku-deploy');
+const runDevelopmentPipeline = require('../pipeline/development-pipeline');
+
+async function deployWithPipeline() {
+    await runDevelopmentPipeline;
+    await rokuDeploy.zipPackage({
+        outDir: 'out',
+        stagingDir: 'out/transpiled',
+        outFile: 'roku-deploy.zip'
+    })
+
+    await rokuDeploy.publish({
+        outDir: 'out',
+        outFile: 'roku-deploy.zip',
+        host: process.env.ROKU_IP,
+        password: process.env.ROKU_DEV_PASSWORD
+    });
+}
+
+module.exports = deployWithPipeline;
+
+if (require.main === module) {
+    deployWithPipeline().catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
+}
